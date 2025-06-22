c1a0a1f6642509b34dc725f3f476303180813a2c
Jian Yi <1211311173@student.mmu.edu.my>
2025-06-22 17:55:36 +0800

    Fix bugs

--- src/main/java/com/EAI/project/Model/Product.java
+++ src/main/java/com/EAI/project/Model/Product.java
@@ -2,6 +2,7 @@ package com.EAI.project.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,18 +24,22 @@ import java.time.OffsetDateTime;
 public class Product {
     @Id
     private String id;
+    @Column(columnDefinition = "text")
     private String title;
+    @Column(columnDefinition = "text")
     private String description;
     private Double priceOriginal;
     private Double priceActual;
     private String deliveryLocation;
     private String category;
+    @Column(columnDefinition = "text")
     private String specification;
     private Double itemRating;
     private Integer totalRating;
     private String sellerName;
     private Integer totalSold;
     private Integer favoriteCount;
+    @Column(columnDefinition = "text")
     private String productLink;
     private String imageLink;
     private String siteName;
